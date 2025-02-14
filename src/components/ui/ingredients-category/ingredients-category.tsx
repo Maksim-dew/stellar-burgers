b1550ba21a3a343648788eac2ed2e78d96bbba64
Maks <maksim.medvedev0307@gmail.com>
2025-02-14 14:12:44 +0300

    Сделал новый ПР

--- src/components/ui/ingredients-category/ingredients-category.tsx
+++ src/components/ui/ingredients-category/ingredients-category.tsx
@@ -8,10 +8,10 @@ export const IngredientsCategoryUI = forwardRef<
   TIngredientsCategoryUIProps
 >(({ title, titleRef, ingredients, ingredientsCounters }, ref) => (
   <>
-    <h3 className='text text_type_main-medium mt-10 mb-6' ref={titleRef}>
+    <h3 className='text text_type_main-medium mt-10 mb-6' ref={titleRef}  >
       {title}
     </h3>
-    <ul className={styles.items} ref={ref}>
+    <ul className={styles.items} ref={ref} data-cy="souce_ingredients">
       {ingredients.map((ingredient) => (
         <BurgerIngredient
           ingredient={ingredient}
